9de10a14e39474dad70fc10b8fed4387dcba6736
mvrtin64 <mvrtingrzsk@gmail.com>
2025-01-25 23:57:44 -0300

    remove cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const terminalRoutes = require('./routes/terminalRoutes');
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 app.use('/api/terminal', terminalRoutes);
